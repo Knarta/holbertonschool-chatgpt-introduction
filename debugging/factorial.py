2f251dd5994c8c4465c8c45e3b078676719e9eeb
root <root@Chacha.>
2025-01-13 18:08:32 +0100

    identified and corrected errors in code samples

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,13 +2,11 @@
 import sys
 
 
-def factorial(n)
-
-
-result = 1
-while n > 1:
-    result *= n
-    n -= 1
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
     return result
 
 
